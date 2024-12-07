1516192d3ec504ce98c948b24d578ca909332edb
Prince Louie <20210713m.princepaquiado.bscs@gmail.com>
2024-12-07 10:54:18 +0800

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import img from '../assets/bg.png';
-import qr from '/UPA-qr.png';
+import qr from '../assets/image.png';
 
 function Home() {
   const [showMoreAbout, setShowMoreAbout] = useState(false);
